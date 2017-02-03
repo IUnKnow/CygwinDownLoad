23d5c143efb9cf38b75d4e92cb395e0d4799e9e7
unknow <414570771@qq.com>
2017-02-03 23:10:52 +0800

    download 有问题

--- cygwindownload.py
+++ cygwindownload.py
@@ -4,8 +4,10 @@ import re
 import os
 
 
-strUrl = 'http://mirrors.163.com/cygwin/x86_64/'
-
+strUrl = 'http://mirrors.163.com/cygwin/x86_64/';
+path = 'H:\\mytest';
+tempUrl = ''.join(strUrl);
+tempName = '';
 #获取html 信息
 def getHtml(strUrl):
     page = urllib.urlopen(strUrl); #打开连接
@@ -26,14 +28,31 @@ def getFileNameList(strHtml):
     return fileNameList; 
 
       
-#def downLoad(listFileName):
+def downLoad(listFileName):
     
+    global tempUrl;
+    global tempName;
+    for name in listFileName:
+        nCount = name.find('/');
+        if nCount == -1:     #nCount 为-1 则是目录 继续进 
+            #print "不是目录下载";
+            print name; 
+        else:
+            #print "是目录继续进入查找文件";
+            temp = tempUrl + name;
+            tempUrl = temp;
+            tempName = name;
+            html = getHtml(temp);
+            listTemp = getFileNameList(html);
+            return downLoad(listTemp);
+    return;
     
-
-      
       
 if __name__ == "__main__":
     
+    
+    #os.mkdir(path);    
     html = getHtml(strUrl);
     listFileName = getFileNameList(html);
-   # downLoad(listFileName);
+    downLoad(listFileName);
+    
